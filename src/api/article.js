a758781bf12ee480b12393325946202259e02cd6
Your Namelnh0771 <you@example.comlnh0771>
2020-01-14 11:00:26 +0800

    封装获取文章方法

---
+++ src/api/article.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+export function getArticles (params) {
+  // 获取推荐文章数据
+  return request({
+    url: 'http://ttapi.research.itcast.cn/app/v1_1/articles',
+    // axios里 query参数放在params里 body参数在data里
+    params: {
+      with_top: 1,
+      ...params
+    }
+  })
+}
