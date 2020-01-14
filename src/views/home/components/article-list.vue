50c126e219a85cfb7b15849ebc2ef8ff49ef8ac8
Your Namelnh0771 <you@example.comlnh0771>
2020-01-14 09:58:05 +0800

    下拉刷新功能实现

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="scroll-wrapper">
+    <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="refreshSucessText">
     <!-- //防止van-list组件，实现上拉加载 -->
     <van-list v-model="upLoading" :finished="finished" finished-text="真的没有了" @load="onLoad">
         <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
     </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -12,9 +14,11 @@ export default {
   name: 'article-list',
   data () {
     return {
-      upLoading: false,
-      finished: false,
-      articles: [] // 空数组接受上拉加载数据
+      upLoading: false, // 上拉加载状态，为true加载
+      finished: false, // 为true表示全部加载
+      articles: [], // 空数组接受上拉加载数据
+      downLoading: false, // 默认不开启下拉刷新
+      refreshSucessText: '更新成功'
     }
   },
   methods: {
@@ -31,6 +35,16 @@ export default {
         } else {
           this.finished = true
         }
+      }, 1000)
+    },
+    // 下拉刷新方法
+    onRefresh () {
+      setTimeout(() => {
+        let arr = Array.from(Array(10), (value, index) => '追加' + (index + 1))
+        // 头部追加数据
+        this.articles.unshift(...arr)
+        this.downLoading = false // 关闭下拉刷新状态
+        this.refreshSucessText = `更新${arr.length}条数据`
       }, 1000)
     }
   }
