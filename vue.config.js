e44051fbda1e605945191b6af25500689329ee6b
Your Namelnh0771 <you@example.comlnh0771>
2020-02-10 18:35:45 +0800

    项目完成

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,15 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  configureWebpack: (config) => {
+    // 这里的config就是vue-cli里面webpack的所有配置信息
+    if (process.env.NODE_ENV === 'production') {
+      // 满足条件表示webpack会在生产模式下运行
+      // 这时就要把所有的console删除掉  然后再打包
+      // 不是删除源代码的console  而是删除打包出来的代码的console----drop_console
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+  publicPath: './' // 默认值是 /  修改为./ 为了使用html协议查看时能读取页面
 }
